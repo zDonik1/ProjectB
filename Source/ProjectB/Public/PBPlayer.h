3edfa922d91796e427f977e198362bd45fe9e579
zDonik <tokhirovdoniyor@gmail.com>
2021-09-03 09:48:09 +0500

    feat: added player class

---
+++ Source/ProjectB/Public/PBPlayer.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PBCharacter.h"
+#include "PBPlayer.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTB_API APBPlayer : public APBCharacter
+{
+	GENERATED_BODY()
+	
+	public:
+	
+};
